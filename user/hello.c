d438eb96bc92bc8bd77b78ca4d9451389c9a7801
Deon Lacambacal <deonlacambacal@gmail.com>
2024-01-17 13:15:29 -0800

    My solution for hello xv6

---
+++ user/hello.c
@@ -0,0 +1,12 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main()
+{
+
+    printf("Hello world, this is Diego Lacambacal\n");
+    printf("I am in section 1002\n\n");
+
+    exit(0);
+}
